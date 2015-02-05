b6ec06cc5c2274544b9957a332e896b2f9f65bf0
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-02-05 15:33:19 +0100

    repository, models base

--- Entity/LanguageRoot.php
+++ Entity/LanguageRoot.php
@@ -3,7 +3,10 @@
 namespace Truelab\KottiMultilanguageBundle\Entity;
 
 use Truelab\DoctrineBundle\ORM\DiscriminatedClassInterface;
+use Truelab\KottiORMBundle\Entity\Document;
 use Truelab\KottiORMBundle\Entity\Node;
+use Truelab\KottiORMBundle\Model\ContentProxyInterface;
+use Truelab\KottiORMBundle\Model\ContentProxyTrait;
 
 /**
  * Class LanguageRoot
@@ -14,10 +17,33 @@ use Truelab\KottiORMBundle\Entity\Node;
  */
 class LanguageRoot extends Node implements DiscriminatedClassInterface
 {
+    /**
+     * @var Document $document
+     */
     protected $document;
 
     public function getDiscriminatorValue()
     {
         return 'language_root';
+    }
+
+    public function getDocumentBody()
+    {
+        return get_class($this->getDocument());
+    }
+
+    public function getDocumentMimeType()
+    {
+        return 'text/html';
+    }
+
+    public function setDocument(Document $document)
+    {
+        $this->document = $document;
+    }
+
+    public function getDocument()
+    {
+        return $this->document;
     }
 }
